999c6f0f4f90b551147e3e10146b609ce13c46dc
umerabbasii890 <140706534+umerabbasii890@users.noreply.github.com>
2023-11-22 22:49:41 +0500

    Initial commit

---
+++ apps.js
@@ -0,0 +1,46 @@
+var a = 5
+// if(a <= 5 ){
+//     console.log("hello")
+// }
+// else{
+//     console.log("no")
+// }
+
+
+
+
+// var s = a <5 ? "hello" : "no"
+// console.log(s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
